4ef4f34161656dff64f5da5741ce4b1e8c38cc88
davydshcherba <davidserba302@gmail.com>
2026-07-26 23:52:45 +0300

    Add Alembic and Python Dotenv dependencies, and remove database initialization from FastAPI lifespan
    This commit introduces Alembic (version 1.18.5) and Python Dotenv (version 1.2.2) as dependencies in the project. Additionally, it removes the database initialization logic from the FastAPI app's lifespan, streamlining the application startup process.

--- app/main.py
+++ app/main.py
@@ -1,21 +1,10 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 
-from app.core.db import init_db
-from app.models import RecipeModel, UserModel  # noqa: F401  ensure models are registered on metadata
 from app.api import user_router
 from app.api import recipes_router
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await init_db()
-    yield
-
-
 app = FastAPI(
         title="FastAPI recipe 🍉",
-        lifespan=lifespan
     )
 
 
